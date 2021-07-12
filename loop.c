350d68ba3608a67876e64594d8605fd0219d86e2
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-07-12 14:38:58 +0530

    third commit

---
+++ loop.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int p,n,count;
+    float r,si;
+    count = 1;
+    while(count<=3)
+    {
+
+
+        printf("enter the value of p,n,r");
+        scanf("%d%d%f",&p,&n,&r);
+        si = p*n*r/100;
+        printf("simple interest = %f",si);
+        count = count +1;
+    }
+        return 0;
+
+
+}
+
