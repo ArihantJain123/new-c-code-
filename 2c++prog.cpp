868b0d428090a7fa221fee4a05e9631e9db0ea36
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-07-18 20:33:02 +0530

    add file

---
+++ 2c++prog.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+
+    double distance,x1,y1,x2,y2;
+    cout<<"enter the value of first coordinate :";
+    cin>>x1>>y1;
+    cout<<"enter the value of second coordinate :";
+    cin>>x2>>y2;
+    distance = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    cout<<" the distance between("<<x1<<","<<y1<<")and("<<x2<<","<<y2<<") two points "<<distance<<endl;
+    return 0;
+
+}
