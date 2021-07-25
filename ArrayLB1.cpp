fbb1a9433634767120bd7b22b34ed4a14ab763df
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-07-25 13:17:01 +0530

    new commit

---
+++ ArrayLB1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+ using namespace std;
+ int  main()
+  {
+      int Arr[10],i;
+
+      //for ( int i=0; i<9; i++)
+        cout<<" enter the value of array element ";
+        for ( int i=0; i<10; i++)
+        {
+            cin>>Arr[i];
+        }
+        cout<<"Array is";
+        for ( int i=0; i<10; i++)
+        {
+            cout<<Arr[i];
+        }
+        return 0;
+
+  }
