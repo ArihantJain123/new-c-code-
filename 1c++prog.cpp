e1f3ddbf816a952b339bb49638a99e94856533cd
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-11-08 17:15:42 +0530

    new

---
+++ 1c++prog.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    system("cls");
+    double rad,area,perameter;
+
+    cout<<"enter the radius of the circle : ";
+    cin>>rad;
+
+    area = 3.14*rad*rad;
+    perameter =  2*3.14*rad;
+    cout<<"area of the circle : ";
+    cout<<area;
+     cout<<"\n perameter of the circle : ";
+     cout<<perameter;
+     return 0;
+
+
+}
